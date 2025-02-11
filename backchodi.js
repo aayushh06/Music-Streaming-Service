212f0f574aba125d73d86d0d142d888fe3f4d7f3
Aayush Sharma <96439480+aayushh06@users.noreply.github.com>
2025-02-11 10:29:04 +0530

    Delete odi.js

--- backchodi.js
+++
@@ -1,7 +0,0 @@
-const songs  = document.querySelectorAll('.card');
-
-songs.forEach(song => {
-    song.addEventListener('click',function(){
-        alert("This Song is currently not available in your region\n Try Again Later or Next time");
-    })
-});
